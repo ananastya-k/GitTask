927d8cca704ceb1b7997c150eb3321359eecd4c1
ananastya-k <nastyakrupovich1998@gmail.com>
2024-08-11 18:22:51 +0300

    B-MajorFeature: Add destruction line at majorFeatureFunction

--- src/Product.java
+++ src/Product.java
@@ -19,6 +19,7 @@ public class Product {
         System.out.println("Restoring the balance of power..");
         System.out.println("Landing on Kashyyyk..");
         System.out.println("Rescuing the Wookiees..");
+        System.out.println("Destruction of the death star..");
 
     }
 
