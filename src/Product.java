1b40724b185d0ccefe27c2ee60d2e895af11f741
ananastya-k <nastyakrupovich1998@gmail.com>
2024-08-11 18:19:59 +0300

    B-Release-v1.0: Add new functional

--- src/Product.java
+++ src/Product.java
@@ -2,8 +2,12 @@ public class Product {
     public static void main(String[] args) {
 
         System.out.println("Version 0.1 without bug");
+
         developFunction();
         majorFeatureFunction();
+
+        System.out.println("New functional");
+
 
     }
 
